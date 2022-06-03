8c8ab4db62b0ad23ddfb04a6446b43ea32f6069d
docherak <docherak@protonmail.ch>
2022-06-03 13:09:51 +0200

    Finish Fahrenheit<=>Celsius conversion tool

--- chapter4/fahrenheit_to_celsius/target/debug/deps/fahrenheit_to_celsius-b4abfc822103677e.d
+++
@@ -1,5 +0,0 @@
-/home/docherak/Documents/rust-exercises/chapter4/fahrenheit_to_celsius/target/debug/deps/fahrenheit_to_celsius-b4abfc822103677e.rmeta: src/main.rs
-
-/home/docherak/Documents/rust-exercises/chapter4/fahrenheit_to_celsius/target/debug/deps/fahrenheit_to_celsius-b4abfc822103677e.d: src/main.rs
-
-src/main.rs:
--- chapter4/fahrenheit_to_celsius/target/debug/fahrenheit_to_celsius.d
+++
@@ -1,1 +0,0 @@
-/home/docherak/Documents/rust-exercises/chapter4/fahrenheit_to_celsius/target/debug/fahrenheit_to_celsius: /home/docherak/Documents/rust-exercises/chapter4/fahrenheit_to_celsius/src/main.rs
