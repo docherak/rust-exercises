32451d00ca148e0dfac0c32e48ee9b1e54f47e54
docherak <docherak@protonmail.ch>
2022-06-03 13:50:22 +0200

    Format Fibonacci file

--- chapter4/fibonacci/target/debug/deps/fibonacci-65f7ec59b8cb7884.d
+++
@@ -1,5 +0,0 @@
-/home/docherak/Documents/rust-exercises/chapter4/fibonacci/target/debug/deps/fibonacci-65f7ec59b8cb7884.rmeta: src/main.rs
-
-/home/docherak/Documents/rust-exercises/chapter4/fibonacci/target/debug/deps/fibonacci-65f7ec59b8cb7884.d: src/main.rs
-
-src/main.rs:
