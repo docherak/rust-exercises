16f0f3c01ad88d29236f71bc1f065fa331116a75
docherak <docherak@protonmail.ch>
2022-06-03 15:07:19 +0200

    Finish lyrics exercise

--- chapter3/lyrics/src/main.rs
+++ chapter3/lyrics/src/main.rs
@@ -1,3 +1,50 @@
 fn main() {
-    println!("Hello, world!");
+    println!("THE TWELVE DAYS OF CHRISTMAS");
+
+    for day in 0..12 {
+        day_intro(day);
+
+        if day == 0 {
+            println!("A partridge in a pear tree");
+            continue;
+        }
+
+        for gift_day in (0..(day + 1)).rev() {
+            if gift_day == 0 {
+                gifts(gift_day, "And ");
+                continue;
+            }
+            gifts(gift_day, "");
+        }
+    }
+}
+
+fn day_intro(day_num: usize) {
+    let days_nums = [
+        "first", "second", "third", "fourth", "fifth", "sixth", "seventh", "eighth", "ninth",
+        "tenth", "eleventh", "twelfth",
+    ];
+
+    println!(
+        "\nOn the {} day of Christmas, my true love sent to me",
+        days_nums[day_num]
+    );
+}
+
+fn gifts(gift_num: usize, prefix: &str) {
+    let gifts = [
+        "a partridge in a pear tree",
+        "Two turtle doves",
+        "Three french hens",
+        "Four calling birds",
+        "Five golden rings",
+        "Six geese a-laying",
+        "Seven swans a-swimming",
+        "Eight maids a-milking",
+        "Nine ladies dancing",
+        "Ten lords a-leaping",
+        "Eleven pipers piping",
+        "Twelve drummers drumming",
+    ];
+    println!("{}{}", prefix, gifts[gift_num]);
 }
